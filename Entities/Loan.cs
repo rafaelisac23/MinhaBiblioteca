80e6a9f1dfe1b43a308714bb185404c7c2bcf937
rafaelisac23 <rafael.darrigo@gmail.com>
2026-03-15 09:18:05 -0300

    Modification_in_loan_entitie

--- Entities/Loan.cs
+++ Entities/Loan.cs
@@ -2,5 +2,21 @@ namespace MinhaBibliotexa.Entities;
 
 public class Loan
 {
-    
+    public int Id  { get; set; }
+    public Person Person { get; set; }
+    public Book Book { get; set; }
+    public DateTime LoanDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+    public  DateTime ExpectedReturnDate { get; set; }
+
+    public Loan(int id, Person person, Book book)
+    {
+        Id = id;
+        Person = person;
+        Book = book;
+        LoanDate = DateTime.Now;
+        ExpectedReturnDate = LoanDate.AddDays(7);
+        ReturnDate = null;
+        
+    }
 }
