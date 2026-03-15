663c4ef784171cc279d663de338d10e11f599779
rafaelisac23 <rafael.darrigo@gmail.com>
2026-03-15 08:38:43 -0300

    Finalizate_First_Menu_version

--- Program.cs
+++ Program.cs
@@ -8,18 +8,38 @@ class Program
 
     static void  ShowMenu()
     {
+        SetConfig();
+        ShowQuad();
+        ShowOptions();
+        Console.SetCursorPosition(10,10);
+        Console.WriteLine("Option: ");
+        Console.SetCursorPosition(17,10);
+        try
+        {
+            int option = int.Parse(Console.ReadLine());
+            if (option > 5)
+            {
+                ShowMenu();
+            }
+            SelectAction(option);
+        }
+        catch (FormatException e)
+        {
+            Console.Clear();
+            Console.WriteLine("The Option is not valid option.");
+            Thread.Sleep(2000);
+            Console.Clear();
+            ShowMenu();
+        }
+       
+        
+    }
+    static void SetConfig()
+    {
         Console.Clear();
         Console.BackgroundColor= ConsoleColor.Black;
         Console.ForegroundColor = ConsoleColor.White;
-        ShowQuad();
-        
-        
-        Console.SetCursorPosition(4,10);
-        Console.WriteLine("Option: ");
-        Console.SetCursorPosition(11,10);
-        int option = int.Parse(Console.ReadLine());
     }
-
     static void ShowQuad()
     {
         Console.Write("┌");
@@ -47,6 +67,25 @@ class Program
         Console.Write("┘");
         
     }
-
+    static void ShowOptions()
+    {
+        Console.SetCursorPosition(8,2);
+        Console.WriteLine("1-Register book");
+        Console.SetCursorPosition(8,3);
+        Console.WriteLine("2-Register Person");
+        Console.SetCursorPosition(8,4);
+        Console.WriteLine("3-Borrow Book");
+        Console.SetCursorPosition(8,5);
+        Console.WriteLine("4-Return Book");
+        Console.SetCursorPosition(8,6);
+        Console.WriteLine("5-List Books");
+        Console.SetCursorPosition(8,7);
+        Console.WriteLine("0-Exit");
+     
+    }
+    static void SelectAction(int option)
+    {
+        
+    }
    
 }
